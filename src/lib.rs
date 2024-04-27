ee5383d095e06da7a7518bc90b8ab6514d50bc47
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-04-27 16:22:30 -0400

    use ephemeral port in integration test
    made run accept a TcpListener so that the test could inject it in there.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 /// Returns an empty OK HttpResonse.
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+/// Returns an HTTP server on that listens  run on the given listener
+///  Currently supported routes
+///   - /health_check -> returns OK and an empty body.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
