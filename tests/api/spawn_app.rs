37c04bb00314744a241e57fab813b2ecbed7b239
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-05-12 22:28:35 -0400

    use password hash instrad of plaintext password

--- tests/api/spawn_app.rs
+++ tests/api/spawn_app.rs
@@ -1,11 +1,43 @@
 use once_cell::sync::Lazy;
 use reqwest::Url;
+use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
 use zero2prod2::configuration::get_configuration;
 use zero2prod2::startup::{get_connection_pool, Application};
 use zero2prod2::telemetry::{get_subscriber, init_subscriber};
+
+pub struct TestUser {
+    pub user_id: Uuid,
+    pub username: String,
+    pub password: String,
+}
+
+impl TestUser {
+    pub fn generate() -> Self {
+        Self {
+            user_id: Uuid::new_v4(),
+            username: Uuid::new_v4().to_string(),
+            password: Uuid::new_v4().to_string(),
+        }
+    }
+
+    async fn store(&self, pool: &PgPool) {
+        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
+        let password_hash = format!("{:x}", password_hash);
+        sqlx::query!(
+            "INSERT INTO users (user_id, username, password_hash)
+            VALUES($1, $2, $3)",
+            self.user_id,
+            self.username,
+            password_hash,
+        )
+        .execute(pool)
+        .await
+        .expect("Failed to store test user.");
+    }
+}
 
 pub struct TestApp {
     /// Address the application is serving HTTP requests from. e.g., localhost:8080.
@@ -16,6 +48,8 @@ pub struct TestApp {
     pub email_server: MockServer,
     /// Application port
     pub port: u16,
+
+    test_user: TestUser,
 }
 
 /// Confirmation links embedded inthe email API.
@@ -38,10 +72,9 @@ impl TestApp {
 
     /// Sends a POST /newsletters with the given body.
     pub async fn post_newsletters(&self, body: &serde_json::Value) -> reqwest::Response {
-        let (username, password) = self.test_user().await;
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
-            .basic_auth(username, Some(password))
+            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
             .send()
             .await
@@ -69,15 +102,6 @@ impl TestApp {
         let html = get_link(&body["content"][0]["value"].as_str().unwrap());
         let plain_text = get_link(&body["content"][1]["value"].as_str().unwrap());
         ConfirmationLinks { html, plain_text }
-    }
-
-    /// Returns username and password of a test user.
-    pub async fn test_user(&self) -> (String, String) {
-        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
-            .fetch_one(&self.db_pool)
-            .await
-            .expect("Failed to fetch test users.");
-        (row.username, row.password)
     }
 }
 
@@ -148,21 +172,9 @@ pub async fn spawn_app() -> TestApp {
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         port: application_port,
+        test_user: TestUser::generate(),
     };
 
-    add_test_user(&test_app.db_pool).await;
+    test_app.test_user.store(&test_app.db_pool).await;
     test_app
-}
-
-async fn add_test_user(pool: &PgPool) {
-    sqlx::query!(
-        "INSERT INTO users (user_id, username, password)
-        VALUES($1, $2, $3)",
-        Uuid::new_v4(),
-        Uuid::new_v4().to_string(),
-        Uuid::new_v4().to_string(),
-    )
-        .execute(pool)
-        .await
-        .expect("Failed tp create test user.");
 }
