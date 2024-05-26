403adf1c5250f178d1d6567380b996c5d3f6ccba
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-05-26 14:55:26 -0400

    cargo fmt and remove debug prints

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -14,12 +14,10 @@ async fn an_error_flash_message_is_set_on_failure() {
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
 
     // Assert
-    eprintln!("wtffff {:?}", flash_cookie);
     assert_eq!(flash_cookie.value(), "Authentication failed");
     assert_is_redirect_to(&response, "/login");
 
     // Act - Part 2, fetch the login page from the redirect.
     let html_page = app.get_login_html().await;
-    eprintln!("got html page {html_page}");
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
