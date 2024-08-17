d84dcdc088596917d5b43c6b743c64b3a8ac7fa0
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-08-17 16:00:24 -0400

    Refactor extracting user id from session 10.9

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,5 +1,5 @@
 //! src/routes/admin/password/post.rs
-use actix_web::{web, HttpResponse};
+use actix_web::{error::InternalError, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
@@ -18,16 +18,25 @@ pub struct FormData {
     new_password_check: Secret<String>,
 }
 
+/// Returns an actix error if the user is not logged in. Otherwise returns the user_id from the
+/// session.
+async fn reject_anonymous_users(session: TypedSession) -> Result<uuid::Uuid, actix_web::Error> {
+    match session.get_user_id().map_err(e500)? {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(actix_web::error::InternalError::from_response(e, response).into())
+        }
+    }
+}
+
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    if user_id.is_none() {
-        return Ok(see_other("/login"));
-    }
-    let user_id = user_id.unwrap();
+    let user_id = reject_anonymous_users(session).await?;
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error("You entered two different passwords - the fields must match.").send();
