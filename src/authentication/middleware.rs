d7dbe2d9b33ee7462e6f97badeee4abefe6deaff
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-08-17 16:27:38 -0400

    Make the authentication middleware add the userId to the request's mutable extenstions.

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,10 +1,30 @@
-use actix_web_lab::middleware::Next;
-use actix_web::body::MessageBody;
-use actix_web::dev::{ServiceRequest, ServiceResponse};
-use actix_web::FromRequest;
-use actix_web::error::InternalError;
+use std::ops::Deref;
+
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
+use actix_web::body::MessageBody;
+use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::error::InternalError;
+use actix_web::{FromRequest, HttpMessage};
+use actix_web_lab::middleware::Next;
+use uuid::Uuid;
+
+#[derive(Copy, Clone, Debug)]
+pub struct UserId(Uuid);
+
+impl std::fmt::Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Deref for UserId {
+    type Target = Uuid;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
@@ -16,7 +36,10 @@ pub async fn reject_anonymous_users(
     }?;
 
     match session.get_user_id().map_err(e500)? {
-        Some(_) => next.call(req).await,
+        Some(user_id) => {
+            req.extensions_mut().insert(UserId(user_id));
+            next.call(req).await
+        }
         None => {
             // User not logged in, they must log in.
             let response = see_other("/login");
