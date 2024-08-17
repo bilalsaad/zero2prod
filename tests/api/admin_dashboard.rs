7de72795eac84772e85f7bf34be5eebe8fe7d1f8
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-08-17 17:09:28 -0400

    Add a send newsletter button to the admin page -- doesn't do anything yet..

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -39,3 +39,21 @@ async fn logout_clears_session_state() {
     let response = app.get_admin_dashboard().await;
     assert_is_redirect_to(&response, "/login");
 }
+
+#[tokio::test]
+async fn admin_dashboard_has_link_to_send_a_newsletter() {
+    // Arrange - Setup app, login and go to the admin dashboard welcome page
+    let app = spawn_app().await;
+
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    });
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    // Act - Go to the dashboard
+    let html_page = app.get_admin_dashboard_html().await;
+    // Verify - There's a link to sending a newsletter.
+    assert!(html_page.contains("Send a newsletter issue"));
+}
