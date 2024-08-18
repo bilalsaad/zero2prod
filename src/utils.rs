5dc60a5ce4b116ba91d7967f181ad4848bc443cc
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-08-18 12:17:51 -0400

    helper error mapper that returns a bad request with the validtion error

--- src/utils.rs
+++ src/utils.rs
@@ -8,6 +8,15 @@ where
     actix_web::error::ErrorInternalServerError(e)
 }
 
+/// Return a 400 with the user-representation of the validation error as body.
+/// while perserving the error's root for logging.
+pub fn e400<T>(e: T) -> actix_web::Error
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static,
+{
+    actix_web::error::ErrorBadRequest(e)
+}
+
 pub fn see_other(location: &str) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((LOCATION, location))
