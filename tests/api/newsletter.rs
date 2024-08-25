37005bc718e9ee00694b1b7d00a7d601b286a33b
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-08-24 20:02:54 -0400

    fix the failing tests - was just some html string mismatch and removing the transient error one as we no longer send on the API call

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -223,14 +223,16 @@ async fn newsletter_creation_is_idempotent() {
 
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_publish_newsletter_html().await;
-    assert!(html_page.contains("<p><i>The newsletter issue has been published!"));
+    dbg!(&html_page);
+    assert!(html_page.contains("The newsletter issue has been accepted"));
 
     // Act - Part 3 - Submit the same requst again
     let response = app.post_newsletters(&request_body).await;
     assert_is_redirect_to(&response, "/admin/newsletter");
     let html_page = app.get_publish_newsletter_html().await;
     dbg!(&html_page);
-    assert!(html_page.contains("The newsletter has been published!"));
+
+    assert!(html_page.contains("The newsletter issue has been accepted"));
 
     app.dispatch_all_pending_emails().await;
     // Mock verifies on Drop that we have sent the email one.
@@ -283,56 +285,4 @@ async fn concurrent_form_submission_is_handled_ok() {
 // Short-hand for a common mocking setup
 fn when_sending_an_email() -> MockBuilder {
     Mock::given(path("/v3/mail/send")).and(method("POST"))
-}
-
-#[tokio::test]
-async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
-    // Arrange
-    let app = spawn_app().await;
-    let newsletter_request_body = serde_json::json!({
-        "title": "Newsletter title",
-        "text_content": "Newsletter body as plain text",
-        "html_content": "<p>Newsletter body as HTML</p>",
-        "idempotency_key": uuid::Uuid::new_v4().to_string()
-    });
-    // Two subscribers instead of one!
-    create_confirmed_subscriber(&app).await;
-    create_confirmed_subscriber(&app).await;
-    app.post_login(&serde_json::json!({
-        "username": &app.test_user.username,
-        "password": &app.test_user.password,
-    }))
-    .await;
-
-    // Part 1 - Submit newsletter form
-    // Email delivery fails for the second subscriber
-    when_sending_an_email()
-        .respond_with(ResponseTemplate::new(200))
-        .up_to_n_times(1)
-        .expect(1)
-        .mount(&app.email_server)
-        .await;
-    when_sending_an_email()
-        .respond_with(ResponseTemplate::new(500))
-        .up_to_n_times(1)
-        .expect(1)
-        .mount(&app.email_server)
-        .await;
-
-    let response = app.post_newsletters(&newsletter_request_body).await;
-    assert_eq!(response.status().as_u16(), 500);
-
-    // Part 2 - Retry submitting the form
-    // Email delivery will succeed for both subscribers now
-    when_sending_an_email()
-        .respond_with(ResponseTemplate::new(200))
-        .expect(1)
-        .named("Delivery retry")
-        .mount(&app.email_server)
-        .await;
-    let response = app.post_newsletters(&newsletter_request_body).await;
-    assert_eq!(response.status().as_u16(), 303);
-
-    app.dispatch_all_pending_emails().await;
-    // Mock verifies on drop...
 }
